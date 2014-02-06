dd2fa6ee6baed78e0766fb919f95bebb36b7858f
KrzysiekF <krzysztof.furtak@gmail.com>
2014-02-06 23:02:15 +0100

    Rewrite plugin

--- kkcountdown/js/build/kkcountdown.js
+++ kkcountdown/js/build/kkcountdown.js
@@ -1,40 +1,103 @@
 (function() {
-  (function($, window) {
-    var KKCountdown;
-    KKCountdown = (function() {
-      KKCountdown.prototype.defaults = {
-        paramA: 'foo',
-        paramB: 'bar'
-      };
+  var KKCountdown,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-      function KKCountdown(el, options) {
-        this.options = $.extend({}, this.defaults, options);
-        this.$el = $(el);
-        this.myMethod('ccc');
+  KKCountdown = (function() {
+    var countdowns, opts;
+
+    KKCountdown.prototype.defaults = {
+      dayText: 'day ',
+      days2Text: 'days ',
+      daysText: 'days ',
+      hourText: ':',
+      hours2Text: 'hours ',
+      hoursText: ':',
+      minutesText: ':',
+      secondsText: '',
+      textAfterCount: '---',
+      oneDayClass: false,
+      displayDays: true,
+      displayZeroDays: true,
+      addClass: false,
+      callback: false,
+      warnSeconds: 60,
+      warnClass: false,
+      rusNumbers: false
+    };
+
+    opts = null;
+
+    countdowns = [];
+
+    function KKCountdown(el, options) {
+      opts = $.extend({}, this.defaults, options);
+      this.$el = $(el);
+      this.prepareHTML();
+      this.countdownInit(this.$el);
+    }
+
+    KKCountdown.prototype.prepareHTML = function() {
+      var box, boxDni, boxDniText, boxGodz, boxGodzText, boxMin, boxMinText, boxSec, boxSecText;
+      box = $(document.createElement('span')).addClass('kkcountdown-box');
+      boxDni = $(document.createElement('span')).addClass('kkc-dni');
+      boxGodz = $(document.createElement('span')).addClass('kkc-godz');
+      boxMin = $(document.createElement('span')).addClass('kkc-min');
+      boxSec = $(document.createElement('span')).addClass('kkc-sec');
+      boxDniText = $(document.createElement('span')).addClass('kkc-dni-text');
+      boxGodzText = $(document.createElement('span')).addClass('kkc-godz-text');
+      boxMinText = $(document.createElement('span')).addClass('kkc-min-text');
+      boxSecText = $(document.createElement('span')).addClass('kkc-sec-text');
+      if (opts.addClass) {
+        box.addClass(opts.addClass);
       }
+      boxGodzText.html(opts.hoursText);
+      boxMinText.html(opts.minutesText);
+      boxSecText.html(opts.secondsText);
+      box.append(boxDni).append(boxDniText).append(boxGodz).append(boxGodzText).append(boxMin).append(boxMinText).append(boxSec).append(boxSecText);
+      return this.$el.append(box);
+    };
 
-      KKCountdown.prototype.myMethod = function(echo) {
-        return this.$el.html(this.options.paramA + ': ' + echo);
-      };
-
-      return KKCountdown;
-
-    })();
-    return $.fn.extend({
-      kkcountdown: function(option) {
-        return this.each(function() {
-          var $this, data;
-          $this = $(this);
-          data = $this.data('kkcountdown');
-          if (!data) {
-            $this.data('kkcountdown', (data = new KKCountdown(this, option)));
-          }
-          if (typeof option === 'string') {
-            return data[option].apply(data, args);
-          }
-        });
+    KKCountdown.prototype.countdownInit = function(obj) {
+      var count, event, now, _ref;
+      count = 0;
+      if (obj.id === void 0) {
+        obj.id = 'kk_' + Math.random(new Date().getTime());
       }
-    });
-  })(window.jQuery, window);
+      if (_ref = obj.id, __indexOf.call(countdowns, _ref) >= 0) {
+        count = countdowns[obj.id];
+      } else {
+        count = obj.data('seconds');
+      }
+      if (count === void 0) {
+        now = new Date();
+        now = Math.floor(now.getTime() / 1000);
+        event = obj.data('time');
+        if (event === void 0) {
+          event = obj.attr('time');
+        }
+        count = event - now;
+      }
+      return typeof console !== "undefined" && console !== null ? console.log(count) : void 0;
+    };
+
+    return KKCountdown;
+
+  })();
+
+  $.fn.extend({
+    kkcountdown: function(option) {
+      return this.each(function() {
+        var $this, data;
+        $this = $(this);
+        data = $this.data('kkcountdown');
+        if (!data) {
+          $this.data('kkcountdown', (data = new KKCountdown(this, option)));
+        }
+        if (typeof option === 'string') {
+          return data[option].apply(data, args);
+        }
+      });
+    }
+  });
 
 }).call(this);
--- kkcountdown/js/kkcountdown.coffee
+++ kkcountdown/js/kkcountdown.coffee
@@ -1,31 +1,102 @@
-(($, window) ->
- 
-  # Define the plugin class
-  class KKCountdown
- 
-    defaults:
-      paramA: 'foo'
-      paramB: 'bar'
- 
-    constructor: (el, options) ->
-      @options = $.extend({}, @defaults, options)
-      @$el = $(el)
-      @myMethod('ccc')
- 
-    # Additional plugin methods go here
-    myMethod: (echo) ->
-      @$el.html(@options.paramA + ': ' + echo)
+# Define the plugin class
+class KKCountdown
 
- 
-  # Define the plugin
-  $.fn.extend kkcountdown: (option) ->
-    @each ->
-      $this = $(this)
-      data = $this.data('kkcountdown')
- 
-      if !data
-        $this.data 'kkcountdown', (data = new KKCountdown(this, option))
-      if typeof option == 'string'
-        data[option].apply(data, args)
- 
-) window.jQuery, window
+  defaults:
+    dayText         :   'day '
+    days2Text       :   'days '
+    daysText        :   'days '
+    hourText        :   ':'
+    hours2Text      :   'hours '
+    hoursText       :   ':'
+    minutesText     :   ':'
+    secondsText     :   ''
+    textAfterCount  :   '---'
+    oneDayClass     :   false
+    displayDays     :   true
+    displayZeroDays :   true
+    addClass        :   false
+    callback        :   false
+    warnSeconds     :   60
+    warnClass       :   false
+    rusNumbers      :   false
+
+  opts = null
+  countdowns = []
+
+  constructor: (el, options) ->
+    opts = $.extend({}, @defaults, options)
+    @$el = $(el)
+
+    @prepareHTML()
+    @countdownInit(@$el)
+
+  
+  prepareHTML:  ->
+    box = $(document.createElement('span')).addClass('kkcountdown-box')
+    boxDni = $(document.createElement('span')).addClass('kkc-dni')
+    boxGodz = $(document.createElement('span')).addClass('kkc-godz')
+    boxMin = $(document.createElement('span')).addClass('kkc-min')
+    boxSec = $(document.createElement('span')).addClass('kkc-sec')
+    boxDniText = $(document.createElement('span')).addClass('kkc-dni-text')
+    boxGodzText = $(document.createElement('span')).addClass('kkc-godz-text')
+    boxMinText = $(document.createElement('span')).addClass('kkc-min-text')
+    boxSecText = $(document.createElement('span')).addClass('kkc-sec-text')
+
+
+    box.addClass(opts.addClass) if opts.addClass
+
+    
+    boxGodzText.html(opts.hoursText)
+    boxMinText.html(opts.minutesText)
+    boxSecText.html(opts.secondsText)
+
+
+    box
+      .append(boxDni)
+      .append(boxDniText)
+      .append(boxGodz)
+      .append(boxGodzText)
+      .append(boxMin)
+      .append(boxMinText)
+      .append(boxSec)
+      .append(boxSecText)
+
+
+    @$el.append(box);
+
+  countdownInit: (obj) -> 
+    count = 0
+
+    #Jesli nie istnieje ID obiektu to tworzymy losowe ID
+    obj.id = 'kk_'+ Math.random( new Date().getTime() ) if obj.id == undefined
+
+    if obj.id in countdowns
+      count = countdowns[obj.id]
+    else
+      count = obj.data('seconds')
+
+    if count == undefined
+      now = new Date()
+      now = Math.floor(now.getTime() / 1000)
+      
+      event = obj.data('time')
+
+      if event == undefined 
+        event = obj.attr('time'); # backward-compatibility
+        
+      count = event - now;
+
+    console?.log(count)
+
+
+# Define the plugin
+$.fn.extend kkcountdown: (option) ->
+  @each ->
+    $this = $(this)
+    data = $this.data('kkcountdown')
+
+    if !data
+      $this.data 'kkcountdown', (data = new KKCountdown(this, option))
+    if typeof option == 'string'
+      data[option].apply(data, args)
+
